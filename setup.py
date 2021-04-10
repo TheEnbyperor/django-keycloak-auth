c6f28294ca9101136c81473b63859351ed4d190a
Q <q@misell.cymru>
2021-04-10 15:40:48 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(name='django-keycloak-auth',
       version='1.0',
       author='Q 🦄',
       author_email='q@magicalcodewit.ch',
-      install_requires=['python-keycloak-client @ git+https://github.com/Peter-Slump/python-keycloak-client#egg=python-keycloak-client'],
+      install_requires=['python-keycloak-client @ git+https://github.com/AS207960/python-keycloak-client.git@22d87577f4637133146c376dbb99553035b340ab#egg=python-keycloak-client'],
       packages=find_packages(),
       package_data={
         "django_keycloak_auth": ["words.txt"]
