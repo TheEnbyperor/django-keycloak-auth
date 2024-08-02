e37532e2b64954dd93079e43880c43fcb5132111
Q <q@misell.cymru>
2024-08-02 21:07:03 +0200

    update deps

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@
 from setuptools import setup, find_packages
 
 setup(name='django-keycloak-auth',
-      version='1.1',
+      version='1.1.1',
       author='Q 🦄',
       author_email='q@magicalcodewit.ch',
-      install_requires=['python-keycloak-client @ git+https://github.com/AS207960/python-keycloak-client.git@20ff6b75e12dbb34d53814ef12e942f08546b6ec#egg=python-keycloak-client'],
+      install_requires=['python-keycloak-client @ git+https://github.com/AS207960/python-keycloak-client.git@0267dfa9021eaf478330dedbce587c7a5016a3b3#egg=python-keycloak-client'],
       packages=find_packages(),
       package_data={
         "django_keycloak_auth": ["words.txt"]
